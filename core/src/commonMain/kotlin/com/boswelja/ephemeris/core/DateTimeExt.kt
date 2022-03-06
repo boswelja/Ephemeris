7053e19fa199fecefb73ae60ecdbb2636e27e430
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-03-06 15:36:26 +1300

    Switch to common implementation of plusMonths and minusDays This means we no longer use expect/actual in the core module

--- core/src/commonMain/kotlin/com/boswelja/ephemeris/core/DateTimeExt.kt
+++ core/src/commonMain/kotlin/com/boswelja/ephemeris/core/DateTimeExt.kt
@@ -2,10 +2,29 @@ package com.boswelja.ephemeris.core
 
 import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.Month
+import kotlinx.datetime.isoDayNumber
+import kotlinx.datetime.number
 
-internal expect fun Month.plusMonths(months: Long): Month
+private val monthCount = Month.values().size
+private val dayOfWeekCount = DayOfWeek.values().size
 
-internal expect fun DayOfWeek.minusDays(days: Long): DayOfWeek
+internal fun Month.plusMonths(months: Long): Month {
+    val newNumber = (this.number + months) % monthCount
+    return if (newNumber > 0) {
+        Month(newNumber.toInt())
+    } else {
+        Month(newNumber.toInt() + monthCount)
+    }
+}
+
+internal fun DayOfWeek.minusDays(days: Long): DayOfWeek {
+    val newNumber = (this.isoDayNumber - days) % dayOfWeekCount
+    return if (newNumber > 0) {
+        DayOfWeek(newNumber.toInt())
+    } else {
+        DayOfWeek(newNumber.toInt() + dayOfWeekCount)
+    }
+}
 
 internal val DayOfWeek.value: Int
     get() = ordinal + 1
