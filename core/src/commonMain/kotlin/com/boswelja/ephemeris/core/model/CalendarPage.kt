5b45169309395a36a156bb7c9794049c56bbf8e1
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-03-24 22:08:18 +1300

    Create CalendarRow and CalendarPage

---
+++ core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarPage.kt
@@ -0,0 +1,5 @@
+package com.boswelja.ephemeris.core.model
+
+public data class CalendarPage(
+    val rows: List<CalendarRow>
+)
---
+++ core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarRow.kt
@@ -0,0 +1,5 @@
+package com.boswelja.ephemeris.core.model
+
+public data class CalendarRow(
+    val days: List<CalendarDay>
+)
