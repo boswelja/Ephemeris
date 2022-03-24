9f744aa549053f5a648c80498a1030e0d7b48f52
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-03-24 23:47:55 +1300

    Comment code

--- core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarPage.kt
+++ core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarPage.kt
@@ -1,5 +1,9 @@
 package com.boswelja.ephemeris.core.model
 
+/**
+ * Contains information about how the calendar should display a page.
+ * @param rows A list of [CalendarRow] for the calendar to display.
+ */
 public data class CalendarPage internal constructor(
     val rows: List<CalendarRow>
 )
--- core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarPageBuilder.kt
+++ core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarPageBuilder.kt
@@ -5,19 +5,33 @@ import kotlinx.datetime.LocalDate
 @DslMarker
 public annotation class CalendarPageDsl
 
+/**
+ * A builder function to create a [CalendarPage]. This is the entrypoint for the calendar page DSL.
+ */
 public fun calendarPage(init: CalendarPageBuilder.() -> Unit): CalendarPage {
     return CalendarPageBuilder().apply(init).build()
 }
 
+/**
+ * A DSL Builder class to construct a [CalendarPage].
+ */
 @CalendarPageDsl
 public class CalendarPageBuilder internal constructor() {
     private val rows = mutableListOf<CalendarRow>()
 
+    /**
+     * Creates a [CalendarRow] on this page. Note rows are ordered from top to bottom, based on
+     * their call order.
+     */
     public fun row(init: CalendarRowBuilder.() -> Unit) {
         val row = CalendarRowBuilder().apply(init).build()
         rows.add(row)
     }
 
+    /**
+     * Creates [count] number of [CalendarRow] on this page. Note rows are ordered from top to
+     * bottom, based on their call order.
+     */
     public fun rows(count: Int, init: CalendarRowBuilder.(Int) -> Unit) {
         repeat(count) {
             val row = CalendarRowBuilder()
@@ -33,15 +47,26 @@ public class CalendarPageBuilder internal constructor() {
     }
 }
 
+/**
+ * A DSL Builder class to construct a [CalendarRow].
+ */
 @CalendarPageDsl
 public class CalendarRowBuilder internal constructor() {
     private val days = mutableListOf<CalendarDay>()
 
+    /**
+     * Creates a [CalendarDay] in this row. Note days are ordered from start to end, based on their
+     * call order.
+     */
     public fun day(init: CalendarDayBuilder.() -> Unit) {
         val day = CalendarDayBuilder().apply(init).build()
         days.add(day)
     }
 
+    /**
+     * Creates [count] number of [CalendarDay] in this row. Note days are ordered from start to end,
+     * based on their call order.
+     */
     public fun days(count: Int, init: CalendarDayBuilder.(Int) -> Unit) {
         repeat(count) {
             val day = CalendarDayBuilder()
@@ -57,13 +82,22 @@ public class CalendarRowBuilder internal constructor() {
     }
 }
 
+/**
+ * A DSL Builder class to construct a [CalendarDay].
+ */
 @CalendarPageDsl
 public class CalendarDayBuilder internal constructor() {
     private var date: LocalDate? = null
     private var focused: Boolean? = null
 
+    /**
+     * Set the date for this [CalendarDay].
+     */
     public fun date(init: () -> LocalDate) { date = init() }
 
+    /**
+     * Sets whether this [CalendarDay] is in focus.
+     */
     public fun focused(init: () -> Boolean) { focused = init() }
 
     internal fun build(): CalendarDay =
