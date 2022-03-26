b249903e37a1ad00b8290a9d3624fa1ef73b6927
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-03-26 18:01:54 +1300

    Implement CalendarState in EphemerisCalendarView.kt

--- android/views/src/main/kotlin/com/boswelja/ephemeris/views/EphemerisCalendarView.kt
+++ android/views/src/main/kotlin/com/boswelja/ephemeris/views/EphemerisCalendarView.kt
@@ -6,23 +6,25 @@ import android.view.LayoutInflater
 import android.widget.FrameLayout
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.boswelja.ephemeris.core.data.CalendarPageSource
 import com.boswelja.ephemeris.core.ui.CalendarPageLoader
+import com.boswelja.ephemeris.core.ui.CalendarState
 import com.boswelja.ephemeris.views.databinding.LayoutViewpagerBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.datetime.LocalDate
 
 class EphemerisCalendarView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr), CalendarState {
 
     private lateinit var coroutineScope: CoroutineScope
 
     private val root = LayoutViewpagerBinding.inflate(LayoutInflater.from(context), this, true)
-
-    private val viewPager: ViewPager2
-        get() = root.root
 
     private var adapter: CalendarPagerAdapter? = null
         set(value) {
@@ -32,14 +34,45 @@ class EphemerisCalendarView @JvmOverloads constructor(
             }
         }
 
-    val pageSource: CalendarPageSource
-        get() = adapter!!.pageLoader.calendarPageSource
-
-    val dayBinder: CalendarDateBinder<*>
-        get() = adapter!!.dayBinder
+    private val viewPager: ViewPager2
+        get() = root.root
 
     private val page: Int
         get() = if (viewPager.currentItem <= 0) Int.MAX_VALUE / 2 else viewPager.currentItem
+
+    private val _displayedDateRange = MutableStateFlow(
+        adapter?.pageLoader?.getDateRangeFor(page) ?: LocalDate(1970, 1, 1)..LocalDate(1970, 1, 1)
+    )
+
+    private val pageChangeCallback = object : OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            adapter?.pageLoader?.getDateRangeFor(page)?.let { _displayedDateRange.tryEmit(it) }
+        }
+    }
+
+    override val displayedDateRange: StateFlow<ClosedRange<LocalDate>> = _displayedDateRange
+
+    override var pageSource: CalendarPageSource
+        get() = adapter!!.pageLoader.calendarPageSource
+        set(value) {
+            adapter!!.pageLoader = CalendarPageLoader(
+                coroutineScope,
+                value
+            )
+        }
+
+    override fun scrollToDate(date: LocalDate) {
+        val page = pageSource.getPageFor(date)
+        viewPager.setCurrentItem(page, false)
+    }
+
+    override suspend fun animateScrollToDate(date: LocalDate) {
+        val page = pageSource.getPageFor(date)
+        viewPager.setCurrentItem(page, true)
+    }
+
+    val dayBinder: CalendarDateBinder<*>
+        get() = adapter!!.dayBinder
 
     init {
         // Attach our height adjuster to handle ViewPager2 height changes
@@ -49,11 +82,13 @@ class EphemerisCalendarView @JvmOverloads constructor(
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         coroutineScope = CoroutineScope(Dispatchers.Default)
+        viewPager.registerOnPageChangeCallback(pageChangeCallback)
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
         coroutineScope.cancel()
+        viewPager.unregisterOnPageChangeCallback(pageChangeCallback)
     }
 
     @Suppress("UNCHECKED_CAST")
