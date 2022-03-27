8282c01c17e95e583c880ebb174e30f6617de8b3
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-03-27 15:56:38 +1300

    Reduce return count in SnapHelperExt.kt

--- android/views/src/main/kotlin/com/boswelja/ephemeris/views/pager/SnapHelperExt.kt
+++ android/views/src/main/kotlin/com/boswelja/ephemeris/views/pager/SnapHelperExt.kt
@@ -3,8 +3,8 @@ package com.boswelja.ephemeris.views.pager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
 
-fun SnapHelper.getSnapPosition(recyclerView: RecyclerView): Int {
-    val layoutManager = recyclerView.layoutManager ?: return RecyclerView.NO_POSITION
-    val snapView = findSnapView(layoutManager) ?: return RecyclerView.NO_POSITION
-    return layoutManager.getPosition(snapView)
+internal fun SnapHelper.getSnapPosition(recyclerView: RecyclerView): Int {
+    return recyclerView.layoutManager?.let { layoutManager ->
+        findSnapView(layoutManager)?.let { snapView -> layoutManager.getPosition(snapView) }
+    } ?: RecyclerView.NO_POSITION
 }
