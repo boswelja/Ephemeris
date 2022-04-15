4d8cfd88b0e0739f00788e116491cfa97cb89484
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-04-15 21:03:08 +1200

    Create InfiniteHorizontalPagerFragment.kt

--- android/views/src/debug/kotlin/com/boswelja/ephemeris/views/InfiniteHorizontalPagerFragment.kt
+++ android/views/src/debug/kotlin/com/boswelja/ephemeris/views/InfiniteHorizontalPagerFragment.kt
@@ -5,18 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.boswelja.ephemeris.views.databinding.FragmentCalendarBinding
+import com.boswelja.ephemeris.views.databinding.FragmentPagerBinding
+import com.boswelja.ephemeris.views.pager.InfiniteHorizontalPager
 
-public class EphemerisCalendarFragment : Fragment() {
-    public lateinit var calendarView: EphemerisCalendarView
+public class InfiniteHorizontalPagerFragment : Fragment() {
+    public lateinit var pager: InfiniteHorizontalPager
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentCalendarBinding.inflate(inflater, container, false)
-        calendarView = binding.calendarView
+        val binding = FragmentPagerBinding.inflate(inflater, container, false)
+        pager = binding.pagerView
         return binding.root
     }
 }
