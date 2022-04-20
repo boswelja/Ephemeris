23ebf5c8846106150f8abd254e49695ce32bcf69
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-04-20 16:37:44 +1200

    Enable SARIF reports for Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.jetbrains.kotlinx.kover") version "0.5.0" apply false
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
 }
 
 group = "io.github.boswelja.ephemeris"
@@ -17,6 +20,24 @@ nexusPublishing {
                 .set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(ossrhUsername)
             password.set(ossrhPassword)
+        }
+    }
+}
+
+// Enable SARIF reports for Detekt
+val detektMergeReport by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
+    output.set(rootProject.buildDir.resolve("reports/detekt/merge.sarif"))
+}
+
+subprojects {
+    plugins.withType(io.gitlab.arturbosch.detekt.DetektPlugin::class) {
+        tasks.create<Detekt>("detektReport") {
+            reports.sarif.required.set(true)
+            finalizedBy(detektMergeReport)
+
+            detektMergeReport.configure {
+                input.from(this@create.sarifReportFile)
+            }
         }
     }
 }
