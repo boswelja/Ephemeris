4422f9b6376696809203dc8ebde5a88c638b4f76
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-04-22 22:28:39 +1200

    Move some more logic to CollapsingCalendarViewModel.kt

--- android/sample/src/main/java/com/boswelja/ephemeris/sample/collapsing/CollapsingCalendarViewModel.kt
+++ android/sample/src/main/java/com/boswelja/ephemeris/sample/collapsing/CollapsingCalendarViewModel.kt
@@ -2,21 +2,66 @@ package com.boswelja.ephemeris.sample.collapsing
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.Flow
+import com.boswelja.ephemeris.core.data.CalendarMonthPageSource
+import com.boswelja.ephemeris.core.data.CalendarPageSource
+import com.boswelja.ephemeris.core.data.CalendarWeekPageSource
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toJavaLocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class CollapsingCalendarViewModel : ViewModel() {
 
-    private val _toggleView = MutableStateFlow(false)
-    val toggle: Flow<Boolean> = _toggleView
+    private val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+
+    private val _calendarExpanded = MutableStateFlow(true)
+    private val _headerText = MutableStateFlow("")
 
     /**
-     * Invert the current state of calendar view
-     * true maps to [com.boswelja.ephemeris.core.data.CalendarMonthPageSource] with [com.boswelja.ephemeris.core.data.DisplayMonthFocusMode]
-     * false maps to [com.boswelja.ephemeris.core.data.CalendarWeekPageSource] with [com.boswelja.ephemeris.core.data.WeekdayFocusMode]
+     * Flow whether the calendar should be expanded.
      */
-    fun toggleView() = viewModelScope.launch {
-        _toggleView.emit(!_toggleView.value)
+    val calendarExpanded: StateFlow<Boolean> = _calendarExpanded
+
+    /**
+     * Flow the text to be displayed in the header.
+     */
+    val headerText: StateFlow<String> = _headerText
+
+    /**
+     * Flows the calendar's current page source.
+     */
+    val calendarPageSource: StateFlow<CalendarPageSource> = calendarExpanded
+        .map { expanded ->
+            if (expanded) {
+                CalendarMonthPageSource(DayOfWeek.SUNDAY)
+            } else {
+                CalendarWeekPageSource(DayOfWeek.SUNDAY)
+            }
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            CalendarMonthPageSource(DayOfWeek.SUNDAY)
+        )
+
+    /**
+     * Toggle the value of [calendarExpanded].
+     */
+    fun toggleCalendarExpanded() = viewModelScope.launch {
+        _calendarExpanded.emit(!_calendarExpanded.value)
+    }
+
+    fun handlePageChanged(displayedDateRange: ClosedRange<LocalDate>) {
+        viewModelScope.launch {
+            val headerText = "${displayedDateRange.start.toJavaLocalDate().format(dateFormatter)} - ${displayedDateRange.endInclusive.toJavaLocalDate().format(dateFormatter)}"
+            _headerText.emit(headerText)
+        }
     }
 }
--- android/sample/src/main/java/com/boswelja/ephemeris/sample/collapsing/views/CollapsingViewsCalendarFragment.kt
+++ android/sample/src/main/java/com/boswelja/ephemeris/sample/collapsing/views/CollapsingViewsCalendarFragment.kt
@@ -4,73 +4,60 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.boswelja.ephemeris.core.data.CalendarMonthPageSource
-import com.boswelja.ephemeris.core.data.CalendarWeekPageSource
 import com.boswelja.ephemeris.sample.R
 import com.boswelja.ephemeris.sample.collapsing.CollapsingCalendarViewModel
 import com.boswelja.ephemeris.sample.databinding.FragmentViewsCalendarBinding
 import com.zhuinden.fragmentviewbindingdelegatekt.viewBinding
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import kotlinx.datetime.DayOfWeek
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.toJavaLocalDate
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
 
 class CollapsingViewsCalendarFragment : Fragment(R.layout.fragment_views_calendar) {
 
     private val binding by viewBinding(FragmentViewsCalendarBinding::bind)
-    private val vm by viewModels<CollapsingCalendarViewModel>()
-    private val dateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+    private val viewModel by viewModels<CollapsingCalendarViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.switchView.setOnClickListener {
-            vm.toggleView()
+            viewModel.toggleCalendarExpanded()
         }
 
+        setupCalendar()
+        setupHeader()
+    }
+
+    private fun setupCalendar() {
         binding.switchCalendar.initCalendar(
-            pageSource = CalendarWeekPageSource(
-                firstDayOfWeek = DayOfWeek.SUNDAY,
-                focusMode = CalendarWeekPageSource.FocusMode.WEEKDAYS
-            ),
+            pageSource = CalendarMonthPageSource(DayOfWeek.SUNDAY),
             dayBinder = CalendarDayBinder()
         )
-
         lifecycleScope.launch {
-            binding.switchCalendar.displayedDateRange.collectLatest {
-                updateHeader(it)
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.calendarPageSource.collectLatest {
+                    binding.switchCalendar.pageSource = it
+                }
             }
         }
-        observeCalendarState()
-    }
-
-    private fun observeCalendarState() {
-        vm.toggle.onEach {
-            val buttonText: String = if (it) {
-                binding.switchCalendar.pageSource = CalendarMonthPageSource(
-                    firstDayOfWeek = DayOfWeek.SUNDAY,
-                    focusMode = CalendarMonthPageSource.FocusMode.MONTH
-                )
-                getString(R.string.toggle_view, getString(R.string.week))
-            } else {
-                binding.switchCalendar.pageSource = CalendarWeekPageSource(
-                    firstDayOfWeek = DayOfWeek.SUNDAY,
-                    focusMode = CalendarWeekPageSource.FocusMode.WEEKDAYS
-                )
-                getString(R.string.toggle_view, getString(R.string.month))
+        lifecycleScope.launch {
+            binding.switchCalendar.displayedDateRange.collectLatest {
+                viewModel.handlePageChanged(it)
             }
-            binding.switchView.text = buttonText
-        }.launchIn(lifecycleScope)
+        }
     }
 
-    private fun updateHeader(dateRange: ClosedRange<LocalDate>) {
-        val headerText = "${dateRange.start.toJavaLocalDate().format(dateFormatter)} - ${dateRange.endInclusive.toJavaLocalDate().format(dateFormatter)}"
-        binding.headerText.text = headerText
+    private fun setupHeader() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.headerText.collectLatest {
+                    binding.headerText.text = it
+                }
+            }
+        }
     }
 }
