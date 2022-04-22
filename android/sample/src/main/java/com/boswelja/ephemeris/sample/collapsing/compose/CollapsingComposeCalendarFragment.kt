8e6bd421a4cac85dced8a32f1cf468a933f8ae93
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-04-22 22:42:29 +1200

    Refactor CollapsingComposeCalendarFragment.kt to use the collapsing ViewModel for data

--- android/sample/src/main/java/com/boswelja/ephemeris/sample/collapsing/compose/CollapsingComposeCalendarFragment.kt
+++ android/sample/src/main/java/com/boswelja/ephemeris/sample/collapsing/compose/CollapsingComposeCalendarFragment.kt
@@ -12,26 +12,25 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.boswelja.ephemeris.compose.EphemerisCalendar
 import com.boswelja.ephemeris.compose.rememberCalendarState
 import com.boswelja.ephemeris.core.data.CalendarMonthPageSource
-import com.boswelja.ephemeris.core.data.CalendarWeekPageSource
+import com.boswelja.ephemeris.core.model.CalendarDay
+import com.boswelja.ephemeris.sample.R
+import com.boswelja.ephemeris.sample.collapsing.CollapsingCalendarViewModel
 import com.boswelja.ephemeris.sample.ui.theme.EphemerisTheme
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.datetime.DayOfWeek
-import kotlinx.datetime.toJavaLocalDate
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
 
 class ComposeCalendarFragment : Fragment() {
     override fun onCreateView(
@@ -51,7 +50,8 @@ class ComposeCalendarFragment : Fragment() {
 
 @Composable
 fun CalendarScreen(
-    headerDateFormatter: DateTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
+    modifier: Modifier = Modifier,
+    viewModel: CollapsingCalendarViewModel = viewModel()
 ) {
     val calendarState = rememberCalendarState {
         CalendarMonthPageSource(
@@ -59,15 +59,20 @@ fun CalendarScreen(
         )
     }
 
-    var headerText by remember { mutableStateOf("") }
+    val headerText by viewModel.headerText.collectAsState()
 
     LaunchedEffect(calendarState) {
         calendarState.displayedDateRange.collectLatest {
-            headerText = "${it.start.toJavaLocalDate().format(headerDateFormatter)} - ${it.endInclusive.toJavaLocalDate().format(headerDateFormatter)}"
+            viewModel.handlePageChanged(it)
+        }
+    }
+    LaunchedEffect(viewModel) {
+        viewModel.calendarPageSource.collectLatest {
+            calendarState.pageSource = it
         }
     }
 
-    Column {
+    Column(modifier) {
         Text(
             text = headerText,
             style = MaterialTheme.typography.titleLarge,
@@ -79,31 +84,34 @@ fun CalendarScreen(
         EphemerisCalendar(
             contentPadding = PaddingValues(horizontal = 16.dp),
             calendarState = calendarState
-        ) { dayState ->
-            Text(
-                modifier = Modifier.padding(16.dp).align(Alignment.Center),
-                text = dayState.date.dayOfMonth.toString(),
-                color = if (dayState.isFocusedDate)
-                    MaterialTheme.colorScheme.primary
-                else
-                    MaterialTheme.colorScheme.onSurface,
-                fontWeight = if (dayState.isFocusedDate) FontWeight.Bold else FontWeight.Normal
+        ) { calendarDay ->
+            CollapsingCalendarDate(
+                calendarDay = calendarDay,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.Center)
             )
         }
         Button(
-            onClick = {
-                if (calendarState.pageSource is CalendarMonthPageSource) {
-                    calendarState.pageSource = CalendarWeekPageSource(
-                        DayOfWeek.SUNDAY
-                    )
-                } else {
-                    calendarState.pageSource = CalendarMonthPageSource(
-                        DayOfWeek.SUNDAY
-                    )
-                }
-            }
+            onClick = viewModel::toggleCalendarExpanded
         ) {
-            Text("Switch")
+            Text(stringResource(R.string.toggle_calendar_view))
         }
     }
+}
+
+@Composable
+internal fun CollapsingCalendarDate(
+    calendarDay: CalendarDay,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        modifier = modifier,
+        text = calendarDay.date.dayOfMonth.toString(),
+        color = if (calendarDay.isFocusedDate)
+            MaterialTheme.colorScheme.primary
+        else
+            MaterialTheme.colorScheme.onSurface,
+        fontWeight = if (calendarDay.isFocusedDate) FontWeight.Bold else FontWeight.Normal
+    )
 }
