f435e20196fd8fafe96add144aef9113a36955b9
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-05-04 19:12:11 +1200

    Provide a more specific message if something goes wrong

--- android/views/src/main/kotlin/com/boswelja/ephemeris/views/EphemerisCalendarView.kt
+++ android/views/src/main/kotlin/com/boswelja/ephemeris/views/EphemerisCalendarView.kt
@@ -76,7 +76,7 @@ public class EphemerisCalendarView @JvmOverloads constructor(
      * view to redraw itself.
      */
     public var dateBinder: CalendarDateBinder<*>
-        get() = _dateBinder!!
+        get() = requireNotNull(_dateBinder) { MissingDateBinderException }
         set(value) {
             @Suppress("UNCHECKED_CAST")
             _dateBinder = value as CalendarDateBinder<RecyclerView.ViewHolder>
@@ -91,7 +91,7 @@ public class EphemerisCalendarView @JvmOverloads constructor(
      * will cause the calendar to recreate it's views.
      */
     public var pageSource: CalendarPageSource
-        get() = _pageLoader!!.calendarPageSource
+        get() = requireNotNull(_pageLoader?.calendarPageSource) { MissingPageSourceException }
         set(value) {
             _pageLoader = CalendarPageLoader(
                 coroutineScope,
@@ -126,19 +126,19 @@ public class EphemerisCalendarView @JvmOverloads constructor(
     }
 
     override fun getPaddingStart(): Int {
-        return currentPager!!.paddingStart
+        return requireNotNull(currentPager?.paddingStart) { GenericCalendarInitException }
     }
 
     override fun getPaddingLeft(): Int {
-        return currentPager!!.paddingLeft
+        return requireNotNull(currentPager?.paddingLeft) { GenericCalendarInitException }
     }
 
     override fun getPaddingEnd(): Int {
-        return currentPager!!.paddingEnd
+        return requireNotNull(currentPager?.paddingEnd) { GenericCalendarInitException }
     }
 
     override fun getPaddingRight(): Int {
-        return currentPager!!.paddingRight
+        return requireNotNull(currentPager?.paddingRight) { GenericCalendarInitException }
     }
 
     override fun setClipToPadding(clipToPadding: Boolean) {
@@ -204,7 +204,7 @@ public class EphemerisCalendarView @JvmOverloads constructor(
             setOnSnapPositionChangeListener { updateDisplayedDateRange(it) }
         }
         addView(newView)
-        updateDisplayedDateRange(currentPager!!.currentPage)
+        updateDisplayedDateRange(newView.currentPage)
     }
 
     /**
@@ -215,5 +215,11 @@ public class EphemerisCalendarView @JvmOverloads constructor(
         displayedDateRangeChangeListener?.let {
             it(displayedDateRange)
         }
+    }
+
+    private companion object {
+        private const val MissingPageSourceException = "No page source found! Did you forget to set up the calendar?"
+        private const val MissingDateBinderException = "No date binder found! Did you forget to set up the calendar?"
+        private const val GenericCalendarInitException = "Calendar not configured correctly"
     }
 }
