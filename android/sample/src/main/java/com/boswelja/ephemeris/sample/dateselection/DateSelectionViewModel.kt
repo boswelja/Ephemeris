b64b386d919afdc507ee0e1a256866e8fd1a033a
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-05-06 00:53:18 +1200

    Create date selection sample This is able to reproduce the page flicker bug in android-views

---
+++ android/sample/src/main/java/com/boswelja/ephemeris/sample/dateselection/DateSelectionViewModel.kt
@@ -0,0 +1,19 @@
+package com.boswelja.ephemeris.sample.dateselection
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.todayAt
+
+class DateSelectionViewModel : ViewModel() {
+    private val _selectedDate = MutableStateFlow(Clock.System.todayAt(TimeZone.currentSystemDefault()))
+
+    val selectedDate: StateFlow<LocalDate> = _selectedDate
+
+    fun selectDate(newDate: LocalDate) {
+        _selectedDate.value = newDate
+    }
+}
