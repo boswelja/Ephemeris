5844c43aed9a196dbe78cdc0fbe203285d89583c
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-05-06 20:55:04 +1200

    Roll back Dokka 1.6.10 appears to be working OK

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.jetbrains.kotlinx.kover") version "0.5.0" apply false
-    id("org.jetbrains.dokka") version "1.6.21" apply false
+    id("org.jetbrains.dokka") version "1.6.10"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("io.gitlab.arturbosch.detekt") version "1.20.0"
 }
