3f19eab6d29012c4b36440d9123ec1c610659805
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-05-06 23:43:09 +1200

    Revert "Fix javadoc generation"
    This reverts commit 33c82a1ce865526dea8438c10117941ccbcb70a7.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.jetbrains.kotlinx.kover") version "0.5.0" apply false
-    id("org.jetbrains.dokka") version "1.6.21" apply false
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("io.gitlab.arturbosch.detekt") version "1.20.0"
+    id("org.jetbrains.dokka")
 }
 
 group = "io.github.boswelja.ephemeris"
