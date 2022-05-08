8a18db2e778f8e6eea18bd0583c4ebec2ebb056b
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-05-08 20:39:51 +1200

    Add some comments to new code

--- core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarPage.kt
+++ core/src/commonMain/kotlin/com/boswelja/ephemeris/core/model/CalendarPage.kt
@@ -9,6 +9,11 @@ import kotlinx.datetime.LocalDate
 public data class CalendarPage internal constructor(
     val rows: List<CalendarRow>
 ) {
+
+    /**
+     * Get the index of the given [date], if this page were to be flattened to a 1d array.
+     * @return -1 if the date was not found on this page, else the index of the date.
+     */
     public fun getFlatIndexOf(date: LocalDate): Int {
         var currIndex = 0
         rows.forEach { row ->
@@ -22,6 +27,10 @@ public data class CalendarPage internal constructor(
         return -1
     }
 
+    /**
+     * Gets the [CalendarDay] at the given flat [index], or the index of the date if this page were
+     * to be flattened to a 1d array. Throws [IllegalStateException] of the index does not exist.
+     */
     public fun getDateForFlatIndex(index: Int): CalendarDay {
         var currIndex = 0
         rows.forEach { row ->
