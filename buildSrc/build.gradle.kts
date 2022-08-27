8a1ace418794276941949ace729d94427412eb39
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-08-27 22:07:44 +1200

    Update Gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-    implementation("com.android.library:com.android.library.gradle.plugin:7.4.0-alpha09")
+    implementation("com.android.library:com.android.library.gradle.plugin:7.4.0-alpha10")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.10")
 }
