db4ba21b690675ac62acd63c8cd58a4ec6a5c628
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-01-28 20:04:49 +1300

    Make some minor optimizations to CalendarPageView

--- android-views/src/main/kotlin/com/boswelja/ephemeris/views/CalendarPageView.kt
+++ android-views/src/main/kotlin/com/boswelja/ephemeris/views/CalendarPageView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.boswelja.ephemeris.core.model.CalendarDay
 import com.boswelja.ephemeris.core.model.CalendarPage
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
@@ -22,7 +23,7 @@ internal class CalendarPageView @JvmOverloads constructor(
         set(value) {
             if (field != value) {
                 field = value
-                if (field != null && calendarDateBinder != null) invalidate()
+                if (field != null && calendarDateBinder != null) postInvalidate()
             }
         }
 
@@ -32,7 +33,7 @@ internal class CalendarPageView @JvmOverloads constructor(
                 field = value
                 dayViewHolderMap.clear()
                 removeAllViews()
-                if (field != null && calendarPage != null) invalidate()
+                if (field != null && calendarPage != null) postInvalidate()
             }
         }
 
@@ -40,55 +41,41 @@ internal class CalendarPageView @JvmOverloads constructor(
     private var dateCellHeightSpec by Delegates.notNull<Int>()
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        // Skip layout if we have no data
+        // Skip measure if we have no data
         if (calendarPage == null || calendarDateBinder == null)
             return super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        when (MeasureSpec.getMode(widthMeasureSpec)) {
-            MeasureSpec.EXACTLY -> {
-                // The calendar should be exactly this width. Split it up and measure each date cell
-                // exactly.
-                val exactWidth = MeasureSpec.getSize(widthMeasureSpec)
-                // Sample the calendar page for the row width
-                val colCount = calendarPage!!.rows.first().days.size
-                val exactDateWidth = exactWidth / colCount
-                dateCellWidthSpec = MeasureSpec.makeMeasureSpec(exactDateWidth, MeasureSpec.EXACTLY)
-            }
+        dateCellWidthSpec = when (val mode = MeasureSpec.getMode(widthMeasureSpec)) {
+            MeasureSpec.EXACTLY,
             MeasureSpec.AT_MOST -> {
-                val maxWidth = MeasureSpec.getSize(widthMeasureSpec)
+                val requestedWidth = MeasureSpec.getSize(widthMeasureSpec)
 
                 // Sample the calendar page for the row width
                 val colCount = calendarPage!!.rows.first().days.size
-                val maxDateWidth = maxWidth / colCount
-                dateCellWidthSpec = MeasureSpec.makeMeasureSpec(maxDateWidth, MeasureSpec.AT_MOST)
+                val maxDateWidth = requestedWidth / colCount
+                MeasureSpec.makeMeasureSpec(maxDateWidth, mode)
             }
+
             else -> {
                 // Assume unspecified
-                dateCellWidthSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+                MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
             }
         }
 
-        when (MeasureSpec.getMode(heightMeasureSpec)) {
-            MeasureSpec.EXACTLY -> {
-                // The calendar should be exactly this width. Split it up and measure each date cell
-                // exactly.
-                val exactHeight = MeasureSpec.getSize(heightMeasureSpec)
-                // Sample the calendar page for the row width
-                val rowCount = calendarPage!!.rows.size
-                val exactDateHeight = exactHeight / rowCount
-                dateCellHeightSpec = MeasureSpec.makeMeasureSpec(exactDateHeight, MeasureSpec.EXACTLY)
-            }
+        dateCellHeightSpec = when (val mode = MeasureSpec.getMode(heightMeasureSpec)) {
+            MeasureSpec.EXACTLY,
             MeasureSpec.AT_MOST -> {
-                val maxHeight = MeasureSpec.getSize(widthMeasureSpec)
+                val requestedHeight = MeasureSpec.getSize(widthMeasureSpec)
 
                 // Sample the calendar page for the row width
                 val rowCount = calendarPage!!.rows.size
-                val maxDateHeight = maxHeight / rowCount
-                dateCellHeightSpec = MeasureSpec.makeMeasureSpec(maxDateHeight, MeasureSpec.AT_MOST)
+                val maxDateHeight = requestedHeight / rowCount
+                MeasureSpec.makeMeasureSpec(maxDateHeight, mode)
             }
+
             else -> {
                 // Assume unspecified
-                dateCellHeightSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+                MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
             }
         }
 
@@ -104,48 +91,21 @@ internal class CalendarPageView @JvmOverloads constructor(
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         if (!changed) return
         if (dayViewHolderMap.isNotEmpty()) {
-            var index = 0
-            calendarPage!!.forEach { rowIndex, colIndex, calendarDay ->
-                val view = dayViewHolderMap[index]!!
-                index++
-
-                val width = view.itemView.measuredWidth
-                val height = view.itemView.measuredHeight
-
-                view.itemView.layout(
-                    width * colIndex,
-                    height * rowIndex,
-                    width * (colIndex + 1),
-                    height * (rowIndex + 1)
-                )
-                @Suppress("UNCHECKED_CAST")
-                (calendarDateBinder as? CalendarDateBinder<ViewHolder>)?.onBindView(view, calendarDay)
+            rebindAllDates {
+                dayViewHolderMap.getValue(it)
             }
         } else {
-            var index = 0
-            calendarPage!!.forEach { rowIndex, colIndex, calendarDay ->
-                val view = calendarDateBinder!!.onCreateViewHolder(layoutInflater, this)
-                view.itemView.measure(dateCellWidthSpec, dateCellHeightSpec)
-                addViewInLayout(
-                    view.itemView,
-                    -1,
-                    view.itemView.layoutParams ?: generateDefaultLayoutParams(),
-                    true
-                )
-                dayViewHolderMap[index] = view
-                index++
-
-                val width = view.itemView.measuredWidth
-                val height = view.itemView.measuredHeight
-
-                view.itemView.layout(
-                    (width * colIndex),
-                    (height * rowIndex),
-                    (width * (colIndex + 1)),
-                    (height * (rowIndex + 1))
-                )
-                @Suppress("UNCHECKED_CAST")
-                (calendarDateBinder as? CalendarDateBinder<ViewHolder>)?.onBindView(view, calendarDay)
+            rebindAllDates {
+                calendarDateBinder!!.onCreateViewHolder(layoutInflater, this).apply {
+                    itemView.measure(dateCellWidthSpec, dateCellHeightSpec)
+                    addViewInLayout(
+                        itemView,
+                        -1,
+                        itemView.layoutParams ?: generateDefaultLayoutParams(),
+                        true
+                    )
+                    dayViewHolderMap[it] = this
+                }
             }
         }
     }
@@ -157,11 +117,35 @@ internal class CalendarPageView @JvmOverloads constructor(
         val endIndex = calendarPage!!.firstDate.until(dates.endInclusive, DateTimeUnit.DAY)
             .coerceAtMost(childCount - 1)
         for (index in startIndex..endIndex) {
+            bindDate(dayViewHolderMap.getValue(index), calendarPage!!.get(index))
+        }
+    }
+
+    private fun bindDate(viewHolder: ViewHolder, calendarDay: CalendarDay) {
+        viewHolder.itemView.post {
             @Suppress("UNCHECKED_CAST")
-            (calendarDateBinder as? CalendarDateBinder<ViewHolder>)!!.onBindView(
-                dayViewHolderMap[index]!!,
-                calendarPage!!.get(index)
+            (calendarDateBinder as? CalendarDateBinder<ViewHolder>)?.onBindView(viewHolder, calendarDay)
+        }
+    }
+
+    private fun rebindAllDates(
+        getOrCreateViewHolder: (Int) -> ViewHolder
+    ) {
+        var index = 0
+        calendarPage!!.forEach { rowIndex, colIndex, calendarDay ->
+            val viewHolder = getOrCreateViewHolder(index)
+            index++
+
+            val width = viewHolder.itemView.measuredWidth
+            val height = viewHolder.itemView.measuredHeight
+
+            viewHolder.itemView.layout(
+                width * colIndex,
+                height * rowIndex,
+                width * (colIndex + 1),
+                height * (rowIndex + 1)
             )
+            bindDate(viewHolder, calendarDay)
         }
     }
 }
